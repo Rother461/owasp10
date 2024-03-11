7ed740afdb4b3b91431c7c50ec6fadc0a3ad856d
piotr <piotrhlawacz@gmail.com>
2024-03-11 21:20:34 +0100

    Added next functional steps

--- src/main/java/com/example/demo/controllers/Loginowanie.java
+++ src/main/java/com/example/demo/controllers/Loginowanie.java
@@ -2,14 +2,20 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Controller
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@RestController
 @RequestMapping("/user")
 public class Loginowanie {
 
@@ -25,5 +31,24 @@ public class Loginowanie {
     public void login(@RequestParam("username") String username, @RequestParam("password") String password) {
         User user = userRepository.login(username, password);
         System.out.println(user.getUsername());
+    }
+
+    @GetMapping("/fetch-data")
+    public String fetchData(@RequestParam("url") String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @PostMapping("/upload")
+    public void uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String content = new BufferedReader(new InputStreamReader(file.getInputStream()))
+                    .lines().collect(Collectors.joining("\n"));
+            JSONObject json = XML.toJSONObject(content);
+            System.out.println(json.toString(4));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
